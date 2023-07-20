5d46872c9caceff3f52e2e235ba4030b137d0849
Luiz Felipe <luizfelipe.ssu@gmail.com>
2023-07-19 21:02:55 -0300

    adicionando tela de login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import LoginScreen from './pages/login';
+import CadastroScreen from './pages/cadastro';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <div>
-      <LoginScreen />
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<LoginScreen />} />
+          <Route path="/cadastro" element={<CadastroScreen />} />
+          
+        </Routes>
+        
+    </Router>
+
   );
 };
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import './login.css';
 
 const LoginScreen = () => {
@@ -24,10 +25,6 @@ const LoginScreen = () => {
     console.log('Password:', password);
   };
 
-  const handleRegister = () => {
-    console.log('Register');
-  };
-
   const eyeIconStyle = {
     position: 'absolute',
     right: '0.5rem',
@@ -40,53 +37,55 @@ const LoginScreen = () => {
   return (
     <div>
       <div className="top-bar">
-        <span className="icon-text">
-            <img className= "icons"src={"/imgs/icons8-brasil-48.png"} alt="Bandeira do Brasil" />
-            <span>Brasil</span>
-        </span>
-        <span style={{ color: 'red' }}>
+        <div style={{ display: 'flex', alignItems: 'center', marginLeft: '35px'}}>
+          <img className="icons" src={"/imgs/icons8-brasil-48.png"} alt="Bandeira do Brasil" />
+          <span className='brasil-span'>Brasil</span>
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', marginLeft: 'auto' }}>
+          <span style={{ color: 'red', marginRight: '20px' }}>
             CORONAVÍRUS (COVID-19)
-        </span>
-        <span>
+          </span>
+          <span style={{ marginRight: '20px' }}>
             Simplifique
-        </span>
-        <span>
+          </span>
+          <span style={{ marginRight: '20px' }}>
             Participe
-        </span>
-        <span>
+          </span>
+          <span style={{ marginRight: '20px' }}>
             Acesso às Informações
-        </span>
-        <span>
+          </span>
+          <span style={{ marginRight: '20px' }}>
             Legislação
-        </span>
-        <span >
+          </span>
+          <span style={{ marginRight: '20px' }}>
             Canais
-        </span>
-        <span>
-          <img style={{marginTop:'3px'}} className= "icons"src={"/imgs/acessibilidade.png"} alt="Acessibilidade" />
-        </span>
+          </span>
+          <span>
+            <img style={{ marginTop: '3px' }} className="icons" src={"/imgs/acessibilidade.png"} alt="Acessibilidade" />
+          </span>
+        </div>
       </div>
-      <div style={{ background: '#EDEDEE', height: 'calc(100vh - 40px)', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',}}>
-        <h1 style={{ position: 'absolute', top: '5px', left: '540px' , color:'#393C47', fontSize:'70px'}}>SAVU</h1>
-        <img src="/imgs/logo.png" alt="Logo" style={{ width: '100px', marginTop: '1rem' ,position: 'absolute', top: '10px', left: '730px', paddingTop:'20px'}} />
-        <h2 style={{ marginTop: '4rem', color:'#393C47'}}>Sistema de Agendamento de Visitas à UFAPE</h2>
-        <div style={{ background: 'white', padding: '1rem', marginTop: '1rem', width: '400px', height:'300px', textAlign: 'center', borderRadius:'10%' }}>
-          <h1 style={{color:'#393C47'}}>Login</h1>
-          <div style={{ fontSize: '20px', textAlign: 'left'}}>Email:</div>
+      <div style={{ background: '#EDEDEE', height: 'calc(100vh - 40px)', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', }}>
+        <h1 style={{ position: 'absolute', top: '5px', left: '540px', color: '#393C47', fontSize: '70px' }}>SAVU</h1>
+        <img src="/imgs/logo.png" alt="Logo" style={{ width: '100px', marginTop: '1rem', position: 'absolute', top: '10px', left: '730px', paddingTop: '20px' }} />
+        <h2 style={{ marginTop: '4rem', color: '#393C47' }}>Sistema de Agendamento de Visitas à UFAPE</h2>
+        <div style={{ background: 'white', padding: '1rem', marginTop: '1rem', width: '400px', height: '300px', textAlign: 'center', borderRadius: '10%' }}>
+          <h1 style={{ color: '#393C47' }}>Login</h1>
+          <div style={{ fontSize: '20px', textAlign: 'left' }}>Email:</div>
           <div style={{ display: 'flex', alignItems: 'center', marginBottom: '0.5rem', position: 'relative' }}>
-              <input type="email" id="email" value={email} onChange={handleEmailChange} className="input-field" style={{ flex: '1', marginRight: '0.5rem', width: '100%', border: 'none', backgroundColor: '#e9e9e9', height:'2rem', borderRadius:'5%',fontSize: '16px'  }} />
+            <input type="email" id="email" value={email} onChange={handleEmailChange} className="input-field" style={{ flex: '1', marginRight: '0.5rem', width: '100%', border: 'none', backgroundColor: '#e9e9e9', height: '2rem', borderRadius: '5%', fontSize: '16px' }} />
           </div>
           <div style={{ fontSize: '20px', textAlign: 'left' }}>Senha:</div>
           <div style={{ display: 'flex', alignItems: 'center', marginBottom: '0.5rem', position: 'relative' }}>
-              <input type={showPassword ? 'text' : 'password'} id="password" value={password} className="input-field" onChange={handlePasswordChange} style={{ flex: '1', marginRight: '0.5rem', paddingRight: '3rem', maxWidth: '100%', backgroundColor:'#e9e9e9', border:'none', height:'2rem', borderRadius:'5%' ,fontSize: '16px' }} />
-              {showPassword ? (
-                  <FaEyeSlash onClick={handleShowPassword} style={eyeIconStyle} />
-              ) : (
-                  <FaEye onClick={handleShowPassword} style={eyeIconStyle} />
-              )}
+            <input type={showPassword ? 'text' : 'password'} id="password" value={password} className="input-field" onChange={handlePasswordChange} style={{ flex: '1', marginRight: '0.5rem', paddingRight: '3rem', maxWidth: '100%', backgroundColor: '#e9e9e9', border: 'none', height: '2rem', borderRadius: '5%', fontSize: '16px' }} />
+            {showPassword ? (
+              <FaEye onClick={handleShowPassword} style={eyeIconStyle} />
+            ) : (
+              <FaEyeSlash onClick={handleShowPassword} style={eyeIconStyle} />
+            )}
           </div>
           <div className="login-buttons">
-            <button onClick={handleRegister} className="button button-register">Cadastrar</button>
+            <Link to="/cadastro" className="button button-register link-no-underline">Cadastrar</Link>
             <button onClick={handleLogin} className="button button-login">Entrar</button>
           </div>
         </div>
