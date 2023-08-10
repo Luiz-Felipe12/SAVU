aa6c4ccb8f58ba84331effbe4d617dc48615956f
Luiz Felipe <luizfelipe.ssu@gmail.com>
2023-08-09 22:07:20 -0300

    finalizando a tela pricipal

--- src/pages/principal.js
+++ src/pages/principal.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaBars, FaUser, FaQuestion, FaPlus, FaCalendarAlt, FaEdit, FaTrashAlt } from 'react-icons/fa';
+import { FaBars, FaUser, FaQuestion, FaPlus, FaCalendarAlt, FaEdit, FaTrashAlt,FaSearch } from 'react-icons/fa';
 
 const PrincipalScreen = () => {
+  const iconStyle = {
+    marginRight: '0.5rem',
+    verticalAlign: 'middle',
+  };
+
+  const handleEditClick = () => {
+    console.log('Clicou em Editar');
+    // Adicione sua lógica de edição aqui
+  };
+
+  const handleDeleteClick = () => {
+    console.log('Clicou em Excluir');
+    // Adicione sua lógica de exclusão aqui
+  };
+
   return (
     <div style={{ background: '#EDEDEE', height: '100vh' }}>
       {/* Top Bar */}
-      <div style={{ background: '#333', color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0.5rem 2rem' }}>
+      <div style={{ background: '#2e3039', color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0.5rem 2rem' }}>
         <div style={{ display: 'flex', alignItems: 'center' }}>
           <FaBars style={{ marginRight: '1rem', cursor: 'pointer' }} />
-          <h2>SAVU</h2>
         </div>
+        <h2>SAVU</h2>
         <div style={{ display: 'flex', alignItems: 'center' }}>
           <FaUser style={{ marginRight: '1rem' }} />
         </div>
@@ -19,59 +34,89 @@ const PrincipalScreen = () => {
       {/* Conteúdo Principal */}
       <div style={{ padding: '2rem' }}>
         {/* Bloco Agendamentos */}
-        <div style={{ background: 'white', padding: '1rem', display: 'flex', alignItems: 'center', marginBottom: '1rem' }}>
-          <div style={{ background: '#e9e9e9', width: '40px', height: '40px', display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%' }}>
-            <FaCalendarAlt />
+        <div style={{ display: 'flex', alignItems: 'center', padding: '1rem', paddingLeft:'0rem' }}>
+          <div style={{ boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.1)', background: 'white', padding: '1rem', marginTop: '0.5rem', marginRight:'1rem', borderRadius: '30%' }}>
+            <FaCalendarAlt size={32} />
           </div>
-          <div style={{ marginLeft: '1rem' }}>
-            <h3>Visitas Agendadas</h3>
-            <p>Histórico dos meus agendamentos de visitas</p>
+          <div>
+            <h1 style={{ margin: 0 }}>Visitas Agendadas</h1>
+            <p style={{ margin: 0,  fontSize:'18px', marginLeft:'0.2rem'}}>Histórico dos meus agendamentos de visitas</p>
           </div>
         </div>
 
         {/* Barra de Pesquisa e Botões */}
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '1rem' }}>
-          <input type="text" placeholder="Pesquisar..." style={{ marginRight: '1rem', padding: '0.5rem', borderRadius: '5px', border: '1px solid #ccc' }} />
-          <div style={{ display: 'flex' }}>
-            <button style={{ marginRight: '0.5rem', background: 'orange', color: 'white', border: 'none', padding: '0.5rem 1rem', borderRadius: '5px' }}>
-              <FaQuestion style={{ marginRight: '0.3rem' }} />
-              Ajuda
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '1rem' }}>
+          <div style={{ display: 'flex', alignItems: 'center', background: 'white', borderRadius: '5px', border: '1px solid #ccc', padding: '0.5rem', marginTop:'1.5rem' }}>
+            <FaSearch style={{ marginLeft: '0.5rem', marginRight: '0.5rem', fontSize: '16px' }} />
+            <input type="text" placeholder="Pesquisar..." style={{ padding: '0.5rem', borderRadius: '5px', border: 'none', outline: 'none', fontSize: '14px' }} />
+          </div>
+          <div style={{ display: 'flex', gap: '0.5rem' }}>
+            <button className="button button-login link-no-underline" style={{ background: '#b3672d', color: 'white', border: 'none', padding: '0.5rem 1rem', borderRadius: '5px', display: 'flex', alignItems: 'center', padding:'0.8rem'}}>
+              <FaQuestion style={{ marginRight: '0.5rem' }} />
+              Tipo de Visitas
             </button>
-            <button style={{ background: '#007BFF', color: 'white', border: 'none', padding: '0.5rem 1rem', borderRadius: '5px', display: 'flex', alignItems: 'center' }}>
-              <FaPlus style={{ marginRight: '0.3rem' }} />
+            <button className="button button-login link-no-underline" style={{ background: '#2e3039', color: 'white', border: 'none', padding: '0.5rem 1rem', borderRadius: '5px', display: 'flex', alignItems: 'center', padding:'0.8rem' }}>
+              <FaPlus style={{ marginRight: '0.5rem' }} />
               Agendar uma visita
             </button>
           </div>
         </div>
 
+
+        <div style={{ boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.1)' }}>
         {/* Tabela de Dados */}
         {/* Cabeçalho */}
-        <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr auto', background: 'white', padding: '1rem', fontWeight: 'bold' }}>
-          <div>Visita</div>
-          <div>Data</div>
-          <div>Horário início</div>
-          <div>Horário término</div>
-          <div>Status</div>
-          <div>Gerenciar</div>
+        <div style={{ display: 'flex', background: 'white', padding: '1rem', fontWeight: 'bold', fontSize: '18px', gap: '1rem', alignItems: 'center', marginTop: '1.5rem' }}>
+          <div style={{ flex: '1' }}>Visita</div>
+          <div style={{ flex: '1' }}>Data</div>
+          <div style={{ flex: '1' }}>Horário início</div>
+          <div style={{ flex: '1' }}>Horário término</div>
+          <div style={{ flex: '1' }}>Status</div>
+          <div style={{ flex: '1', textAlign: 'center' }}>Gerenciar</div>
+          
         </div>
 
         {/* Linha de Dados (Exemplo) */}
-        <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr auto', background: 'white', padding: '1rem', borderBottom: '1px solid #ccc' }}>
-          <div>Visita 1</div>
-          <div>2023-07-19</div>
-          <div>09:00 AM</div>
-          <div>11:00 AM</div>
-          <div>Agendada</div>
-          <div style={{ display: 'flex' }}>
-            <button style={{ marginRight: '0.5rem' }}>
-              <FaEdit />
-            </button>
-            <button>
-              <FaTrashAlt />
-            </button>
+        <div style={{ display: 'flex', flexDirection: 'column', background: 'white', padding: '1rem', gap: '1rem', fontSize: '16px' }}>
+          {/* Linha de Dados */}
+          <div style={{ display: 'flex', alignItems: 'center', borderBottom: '1px solid #ccc', padding: '0.5rem 0' }}>
+            <div style={{ flex: '1' }}>Visita 1</div>
+            <div style={{ flex: '1' }}>07-08-2023</div>
+            <div style={{ flex: '1' }}>09:00 AM</div>
+            <div style={{ flex: '1' }}>11:00 AM</div>
+            <div style={{ flex: '1' }}>Agendada</div>
+            <div style={{ flex: '1', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <button onClick={handleEditClick} style={{ border: 'none', background: 'none', cursor: 'pointer', marginLeft: '1rem' }}>
+                <FaEdit size={20} style={iconStyle} />
+              </button>
+              <button onClick={handleDeleteClick} style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
+                <FaTrashAlt size={20} style={iconStyle} />
+              </button>
+            </div>
+          </div>
+
+
+          {/* Outra Linha de Dados */}
+          <div style={{ display: 'flex', alignItems: 'center', borderBottom: '1px solid #ccc', padding: '0.5rem 0' }}>
+            <div style={{ flex: '1' }}>Visita 2</div>
+            <div style={{ flex: '1' }}>08-08-2023</div>
+            <div style={{ flex: '1' }}>10:00 AM</div>
+            <div style={{ flex: '1' }}>12:00 PM</div>
+            <div style={{ flex: '1' }}>Concluída</div>
+            <div style={{ flex: '1', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <button onClick={handleEditClick} style={{ border: 'none', background: 'none', cursor: 'pointer', marginLeft: '1rem' }}>
+                <FaEdit size={20} style={iconStyle} />
+              </button>
+              <button onClick={handleDeleteClick} style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
+                <FaTrashAlt size={20} style={iconStyle} />
+              </button>
+            </div>
           </div>
         </div>
+        </div>
       </div>
+
+        
     </div>
   );
 };
